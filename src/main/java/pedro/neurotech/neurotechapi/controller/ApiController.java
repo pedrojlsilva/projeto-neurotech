49956fdbf212348d280ad944f6e2c77d98dacf68
Pedro Jorge Silva <pjls2@cin.ufpe.br>
2022-08-14 10:57:18 -0300

    Allowing CORS

--- src/main/java/pedro/neurotech/neurotechapi/controller/ApiController.java
+++ src/main/java/pedro/neurotech/neurotechapi/controller/ApiController.java
@@ -4,14 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import pedro.neurotech.neurotechapi.domain.CurrencyRate;
+import org.springframework.web.bind.annotation.*;
+import pedro.neurotech.neurotechapi.model.CurrencyRate;
 import pedro.neurotech.neurotechapi.service.CurrencyRateService;
 
-import java.sql.Date;
 import java.util.List;
 
 @RestController
@@ -23,11 +19,12 @@ public class ApiController {
     //Controle dos endpoints
     private final CurrencyRateService currencyRateService;
 
+    @CrossOrigin
     @GetMapping(path="latest")
     public ResponseEntity<CurrencyRate> getLatestCurrency(){
         return new ResponseEntity<CurrencyRate>(currencyRateService.getLatest(),HttpStatus.OK);
     }
-
+    @CrossOrigin
     @GetMapping(path="interval")
     public ResponseEntity<List<CurrencyRate>> getCurrencyWithInterval(@RequestParam(defaultValue = "1996-08-15", name = "startDate") String startDate, @RequestParam(defaultValue = "2022-08-15", name="endDate") String endDate){
         return new ResponseEntity<List<CurrencyRate>>(currencyRateService.getRatesBetweenDates(startDate, endDate),HttpStatus.OK);
